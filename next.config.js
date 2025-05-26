b27cc10e01621fd576323fac98660fb2e94d4040
Altahanaian <thanaian@hotmail.com>
2025-05-26 17:45:50 +0300

    Initial commit for SMSCO AI 2025 platform

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: {
-    locales: ['en', 'ar'],
-    defaultLocale: 'en',
-    localeDetection:true,
+  images: {
+    formats: ['image/webp'],
+    minimumCacheTTL: 60 * 60 * 24, // 1 يوم
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.smsco.ai',
+      },
+    ],
   },
 };
 
